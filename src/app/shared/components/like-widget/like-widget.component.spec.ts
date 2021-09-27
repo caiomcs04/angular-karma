60ae83413ab0405f106e33236ff60a011276b086
caiomcs04 <caio6c@yahoo.com.br>
2021-09-27 17:20:38 -0300

    testes fixture

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 
 import { LikeWidgetComponent } from './like-widget.component';
+import { LikeWidgetModule } from './like-widget.module';
 
-describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+describe(LikeWidgetComponent.name, () => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
-    })
-    .compileComponents();
-  });
+      imports: [LikeWidgetModule]
+    }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LikeWidgetComponent);
+    fixture = TestBed.createComponent(LikeWidgetComponent)
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should auto generate ID when id input prop is missing', () => {
+    //por padrao nao é detectado mudança de estado pelo angular
+    //para detectar mudanças devemos utilizar o detectChanges do fixture
+    //ele é responsavel por disparar o ngOnInit
+    //é possivel fazer de forma automatica passando 
+    //a responsabilidade para o provider fixture. porém não é recomendavel que se faça isso
+    fixture.detectChanges();
+
+    expect(component.id).toBeTruthy();
+  })
+
+  it('Should auto generate ID when id input prop is present', () => {
+    const someId = 'someId';
+    component.id = someId
+    fixture.detectChanges();
+
+    expect(component.id).toBeTruthy();
+  })
 });
